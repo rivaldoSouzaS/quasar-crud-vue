5e5655aa5e02eb29a4e55a09b63370422cdbe7d6
rivaldoSouzaS <rivaldosilva@si.fiponline.edu.br>
2022-10-20 14:15:36 -0300

    creat remove post

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,26 +1,29 @@
 <template>
   <q-page padding>
-    <q-table
-          title="Treats"
-          :rows="posts"
-          :columns="columns"
-          row-key="name"
-        />
+    <q-table title="Treats" :rows="posts" :columns="columns" row-key="name">
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          <q-btn icon="delete" color="negative" dense size="sm" @click="handleDeletePost(props.row.id)" />
+        </q-td>
+      </template>
+    </q-table>
   </q-page>
 </template>
 
 <script>
 import { defineComponent, ref, onMounted } from 'vue'
-import { api } from 'boot/axios'
+import postsService from 'src/services/posts'
 
 export default defineComponent({
   name: 'IndexPage',
   setup () {
     const posts = ref([])
+    const { list, remove } = postsService()
     const columns = [
       { name: 'id', field: 'id', label: 'Id', sortable: true, align: 'left' },
       { name: 'title', field: 'title', label: 'Title', sortable: true, align: 'left' },
-      { name: 'author', field: 'author', label: 'Author', sortable: true, align: 'left' }
+      { name: 'author', field: 'author', label: 'Author', sortable: true, align: 'left' },
+      { name: 'actions', field: 'actions', label: 'Acões', align: 'right' }
     ]
 
     onMounted(() => {
@@ -29,16 +32,27 @@ export default defineComponent({
 
     const getPosts = async () => {
       try {
-        const { data } = await api.get('posts')
+        const data = await list()
         posts.value = data
         console.log(posts.value)
       } catch (error) {
         console.error(error)
       }
     }
+
+    const handleDeletePost = async (id) => {
+      try {
+        await remove(id)
+        alert('Sucesso')
+        await getPosts()
+      } catch (error) {
+        alert(error)
+      }
+    }
     return {
       posts,
-      columns
+      columns,
+      handleDeletePost
     }
   }
 })
